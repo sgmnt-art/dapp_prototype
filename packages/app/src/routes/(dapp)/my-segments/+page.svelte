0028da8e8b53cd893ee9604fd0d8f0919ea46aa1
Nicolai <nicolai.verbaarschot@gmail.com>
2023-04-28 12:04:21 +0200

    addded bottom margin on my collection.

--- packages/app/src/routes/(dapp)/my-segments/+page.svelte
+++ packages/app/src/routes/(dapp)/my-segments/+page.svelte
@@ -5,7 +5,7 @@
 
 <h1 class="mt-20 mb-20 text-[32pt] font-bold italic">My Collection</h1>
 
-<div class="mt-20 grid h-fit w-full grid-cols-[repeat(auto-fit,300px)] justify-center gap-4">
+<div class="mt-20 grid h-fit w-full grid-cols-[repeat(auto-fit,300px)] justify-center gap-4 mb-20">
   <NFTcard zoomLevel={[2.5, 0.84, 2.13]} />
   <NFTcard zoomLevel={[0.825, 0, 2.13]} />
   <NFTcard zoomLevel={[0, -0.835, 2.13]} />
